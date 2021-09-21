50c9012bae3d7be164c4006cd11bcc0c7c9e7533
EC2 Default User <ec2-user@ip-172-31-41-171.us-east-2.compute.internal>
2021-09-21 20:04:27 +0000

    scheduling

--- prod-1/3-sched/test.py
+++ prod-1/3-sched/test.py
@@ -16,13 +16,13 @@ for tindex2, tindex1 in enumerate(test_indices):
 
     passes = [index for index, state in enumerate(constellation) if expression.evaluate(state)]
 
-    print('%3d : %3d %1d : %3d %3d' % (
-        tindex2, 
-        tindex1, 
-        problem.test_groups[tindex1],
-        len(passes), 
-        count,
-        ))
+#    print('%3d : %3d %1d : %3d %3d' % (
+#        tindex2, 
+#        tindex1, 
+#        problem.test_groups[tindex1],
+#        len(passes), 
+#        count,
+#        ))
 
     
 test_array = np.zeros((10, 100))
@@ -34,19 +34,29 @@ car_array[...] = -1
 time_index = 0
 slot_index = 0
 
+unscheduled_tests = np.full(10,-1)
+
 for test_index in test_indices:
+    print('test_index')
+    print(test_index)
 
     expression = problem.test_set.expressions[test_index]
     count = problem.test_set.counts[test_index]
+    print('count')
+    print(count)
 
     car_candidates = [index for index, state in enumerate(constellation) if expression.evaluate(state)]
 
     ncar = 0
     
     for car_index in car_candidates:
-        
+        print('car_index in car_candidates')
+        print(car_index)
+
         if car_index in car_array[time_index, :slot_index]: continue
 
+        print('slot_index = %d' % slot_index)
+        print('time_index = %d' % time_index)
         test_array[slot_index, time_index] = test_index
         car_array[slot_index, time_index] = car_index
 
@@ -55,17 +65,25 @@ for test_index in test_indices:
         if ncar == count: break
 
         slot_index += 1
-        if slot_index == test_indices.shape[0]:
+        #if slot_index == test_indices.shape[0]:
+        if slot_index == 9:
             slot_index = 0
             time_index += 1
 
+    print('final number of cars')
+    print(ncar)
     print(car_array[:, :7])
     print(car_candidates)
+    print('')
+    print('')
 
+    index_unscheduled = 0
     if ncar < count: 
-        raise RuntimeError('Cannot satisfy test')
-
-        
+        #raise RuntimeError('Cannot satisfy test')
+        unscheduled_tests[index_unscheduled] = test_index
+        print('unscheduled_tests') 
+        print(unscheduled_tests)
+        index_unscheduled += 1
 
     
     
