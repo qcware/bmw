aedd6129c0d08c8f730e32deb8a3a340cf19b387
alsaadon <alsaadonr@gmail.com>
2021-11-18 13:36:26 -0500

    experiment 2 results

---
+++ prod-1/2p-prod/verify/data_visualization/plot_hamming.py
@@ -0,0 +1,29 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+sol = np.load('../../test-6.npz')
+constellation = sol['constellation']
+constellation_type_indices = sol['constellation_type_indices']
+
+
+x = np.zeros(62,dtype=int)
+y = np.zeros(62,dtype=int)
+for car_i in range(62):
+    x[car_i] = car_i
+    y[car_i] = sum(constellation[car_i][:])
+
+plt.rcParams.update({'font.size': 8})
+
+plt.clf()
+#plt.plot(x,y,'o', c='mediumblue', markersize=2)
+plt.bar(x,y)
+plt.axis([-1, 62, 50, 100])
+plt.yticks(np.arange(0, 100+1, 10))
+#axes=plt.gca()
+#axes.set_aspect('equal')
+plt.xlabel('Test Car')
+plt.ylabel('Hamming Weight')
+#plt.legend(loc=1)
+plt.savefig('hamming-6.pdf',bbox_inches='tight')
+
---
+++ prod-1/2p-prod/verify/data_visualization/plot_rules.py
@@ -0,0 +1,43 @@
+import bmw 
+import numpy as np
+import matplotlib.pyplot as plt 
+
+problem = bmw.Problem.parse(filepath='../../../../data/3-refined')
+
+dat = np.load('../../test-6.npz')
+constellation = dat['constellation']
+constellation_type_indices = dat['constellation_type_indices']
+
+
+constellation = constellation[:62, :]
+constellation_type_indices = constellation_type_indices[:62]
+
+metric = np.array([10 * problem.test_groups[index] - problem.test_set.counts[index] for index in range(len(problem.test_groups))])
+test_indices = list(np.argsort(metric))
+
+
+x = np.zeros(62,dtype=int)
+for index in range(62):
+    x[index] = index
+
+
+y = np.zeros(62,dtype=int)
+for t2, test_index in enumerate(test_indices):
+
+    expression = problem.test_set.expressions[test_index]
+
+    car_candidates = [index for index, state in enumerate(constellation) if expression.evaluate(state)]
+    for ii, value in enumerate(car_candidates):
+        y[value] += 1
+
+
+plt.rcParams.update({'font.size': 8})
+plt.clf()
+plt.bar(x,y,color='r')
+plt.axis([-1, 62, 50, 250])
+plt.yticks(np.arange(0, 250+1, 50))
+plt.xlabel('Test Car')
+plt.ylabel('Number of Test Rules Satisfied')
+plt.savefig('rules-6.pdf',bbox_inches='tight')
+
+
